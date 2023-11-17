df03fffaafe6fe588ce4f44042d1942d1d1f7440
Monica Barboza <145809292+MonBoza@users.noreply.github.com>
2023-11-17 13:55:57 -0600

    WIP

--- src/currencyAPI.js
+++ src/currencyAPI.js
@@ -1,9 +1,10 @@
 export default class CurrencyExchange {
-    static async getExchangeRate(USD) {
+    static async getExchangeRate(USD, selectedCurrencies) {
         try{
-            const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD=${USD}`);
+            const currenciesSelected = selectedCurrencies.join(",");
+            const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD=${USD}&targets=${currenciesSelected}`);
             const jsonResponse = await response.json();
-            if(!response.ok) {
+            if(response && response.conversion_rates) {
                 const errorMessage = `${response.status} ${response.statusText} ${response.message}`;
                 throw new Error(errorMessage);
             }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './css/styles.css';
 import CurrencyExchange from './currencyAPI';
 
 // Business Logic
-async function getExchangeRate(USD) {
+async function getExchangeRate(USD, selectedCurrencies) {
     const response = await CurrencyExchange.getExchangeRate(USD);
     if (response) {
         printElements(response, USD);
@@ -16,26 +16,30 @@ async function getExchangeRate(USD) {
 // UI
 
 function printElements(response, USD) {
-    document.querySelector('displayResults').innerText = `The exchange rate for ${USD} :`;
-    const resultsContainer = document.querySelectorAll('displayResults');
-    response.forEach(function (currency){
-        const listItem = document.createElement("li");
-        listItem.textContent = `${currency.conversion_Rates}`;
-        resultsContainer.appendChild(listItem);
+    document.querySelector('#displayResults').innerText = `The exchange rate for ${USD} :`;
+    const resultsContainer = document.querySelector('#displayResults');
+    const conversionRatesArray = response.conversion_rates;
+    console.log(response[8])
+    conversionRatesArray.forEach(entry => {
+        const [currency, rate] = entry
+        const listItems = document.createElement("li");
+        listItems.textContent =  `${currency}: ${rate}`
+        resultsContainer.appendChild(listItems);
     });
 } 
 
 function printError(error, USD) {
-    document.querySelector('#showResponse').innerText =`There was an error accessing the currency exchange rate for ${USD} ${error}.`;
+    document.querySelector('#showResponse').innerHTML =`There was an error accessing the currency exchange rate for ${USD} ${error}.`;
 }
 
 function handleFormSubmission(event) {
     event.preventDefault();
-    const USD = document.querySelector('USD').value;
-    document.querySelectorAll('form-check-input').value;
-    getExchangeRate(USD);
+    const USD = document.querySelector('#USD').value;
+    const selectedCurrencies = Array.from(document.querySelectorAll("input[name='currency']:checked")).map(checkbox => checkbox.value);
+    document.querySelectorAll("input[name='currency']:checked");
+    getExchangeRate(USD, selectedCurrencies);
 
 }
 window.addEventListener("load", function () {
-    document.querySelectorAll('form').addEventListener('submit', handleFormSubmission);
+    document.querySelector('#exchangeForm').addEventListener('submit', handleFormSubmission);
 });
