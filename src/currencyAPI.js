115a05042f6794cb2a79dcadcbb9f56c6f781f29
Monica Barboza <145809292+MonBoza@users.noreply.github.com>
2023-11-17 12:17:33 -0600

    WIP updating UI logic

--- src/currencyAPI.js
+++ src/currencyAPI.js
@@ -4,7 +4,7 @@ export default class CurrencyExchange {
             const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD=${USD}`);
             const jsonResponse = await response.json();
             if(!response.ok) {
-                const errorMessage = `${response.status} ${response.statusText} ${response.message}`
+                const errorMessage = `${response.status} ${response.statusText} ${response.message}`;
                 throw new Error(errorMessage);
             }
             return jsonResponse;
--- src/index.js
+++ src/index.js
@@ -9,12 +9,33 @@ async function getExchangeRate(USD) {
     if (response) {
         printElements(response, USD);
     } else {
-        printError(response, keyword);
+        printError(response, USD);
     }
 }
 
 // UI
 
 function printElements(response, USD) {
-    document.querySelector('displayResults').innerText = `The exchange rate for ${USD}`
+    document.querySelector('displayResults').innerText = `The exchange rate for ${USD} :`;
+    const resultsContainer = document.querySelectorAll('displayResults');
+    response.forEach(function (currency){
+        const listItem = document.createElement("li");
+        listItem.textContent = `${currency.conversion_Rates}`;
+        resultsContainer.appendChild(listItem);
+    });
 } 
+
+function printError(error, USD) {
+    document.querySelector('#showResponse').innerText =`There was an error accessing the currency exchange rate for ${USD} ${error}.`;
+}
+
+function handleFormSubmission(event) {
+    event.preventDefault();
+    const USD = document.querySelector('USD').value;
+    document.querySelectorAll('form-check-input').value;
+    getExchangeRate(USD);
+
+}
+window.addEventListener("load", function () {
+    document.querySelectorAll('form').addEventListener('submit', handleFormSubmission);
+});
