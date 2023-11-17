3d1b3803fb92b6b7b8b1f6facdeb6d8d9fa87757
Monica Barboza <145809292+MonBoza@users.noreply.github.com>
2023-11-17 11:36:22 -0600

    WIP UI logic

--- src/currencyAPI.js
+++ src/currencyAPI.js
@@ -1,7 +1,7 @@
 export default class CurrencyExchange {
-    static async getExchangeRate(total) {
+    static async getExchangeRate(USD) {
         try{
-            const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD=${total}`);
+            const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD=${USD}`);
             const jsonResponse = await response.json();
             if(!response.ok) {
                 const errorMessage = `${response.status} ${response.statusText} ${response.message}`
--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import CurrencyExchange from './currencyAPI';
+
+// Business Logic
+async function getExchangeRate(USD) {
+    const response = await CurrencyExchange.getExchangeRate(USD);
+    if (response) {
+        printElements(response, USD);
+    } else {
+        printError(response, keyword);
+    }
+}
+
+// UI
+
+function printElements(response, USD) {
+    document.querySelector('displayResults').innerText = `The exchange rate for ${USD}`
+} 
